694816084254afac964852114d876f4ece3cbab0
ishika1234-sudo <Ishika.Bhattacharya@lntinfotech.com>
2021-12-26 11:39:51 +0530

    changes made in init.py

--- __init__.py
+++ __init__.py
@@ -5,14 +5,14 @@ from flask_login import login_required,current_user,login_manager,LoginManager
 
 def config():
     app = Flask(__name__)
-    app.config['MYSQL_HOST'] = '127.0.0.1'
+    app.config['MYSQL_HOST'] = 'host'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'oldmonkrsk5@'
+    app.config['MYSQL_PASSWORD'] = 'my password'
     app.config['MYSQL_DB'] = 'quizapplicaton'
     app.config['charset'] = "utf-8"
     app.app_context().push()
     mysql = MySQL(app)
-    app.secret_key = 'oldmonkrsk5'
+    app.secret_key = 'secret key here'
 
     from profile import profile_blueprint as profile_blueprint
     app.register_blueprint(profile_blueprint)
